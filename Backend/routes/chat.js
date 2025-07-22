f080bbd212d400823f317b979b4dc0309df358f7
JashwanthYerra2404 <jashwanthyerra2404@gmail.com>
2025-07-22 21:30:12 +0530

    backend completed

--- Backend/routes/chat.js
+++ Backend/routes/chat.js
@@ -1,8 +1,10 @@
 import express from "express";
 import Thread from "../models/Thread.js"; 
+import getAiResponse from "../utils/Gemini.js";
 
 const router = express.Router();
 
+// Test route to create a thread
 router.post("/test", async (req, res) => {
     try{
         const thread = new Thread({
@@ -18,6 +20,7 @@ router.post("/test", async (req, res) => {
     }
 });
 
+// Route to get all threads
 router.get("/thread", async(req, res) => {
     try{
         const threads = await Thread.find({}).sort({ updatedAt: -1 });
@@ -29,6 +32,7 @@ router.get("/thread", async(req, res) => {
     }
 });
 
+// Route to get a specific thread by ID
 router.get("/thread/:threadId", async (req, res) => {
     try{
         const thread = await Thread.findOne({ threadId: req.params });
@@ -43,6 +47,7 @@ router.get("/thread/:threadId", async (req, res) => {
     }
 });
 
+// Route to delete a thread
 router.delete("/thread/:threadId", async (req, res) => {
     try{
         const deletedthread = await Thread.findOneAndDelete({ threadId: req.params });
@@ -57,5 +62,43 @@ router.delete("/thread/:threadId", async (req, res) => {
     }
 });
 
+// Route to handle chat messages
+router.post("/chat", async (req, res) => {
+    const { threadId, message } = req.body;
+
+    if(!threadId || !message) {
+        return res.status(400).json({ error: 'Thread ID and message are required' });
+    }
+
+    try {
+        const thread = await Thread.findOne({ threadId: threadId});
+
+        if(!thread) {
+            // Create a new thread if it doesn't exist
+            thread = new Thread({
+                threadId: threadId,
+                title: message.substring(0, 50),
+                messages: [{role: 'user', content: message}]
+            });
+        }
+        else {
+            // Update existing thread
+            thread.messages.push({ role: 'user', content: message });
+            thread.updatedAt = Date.now();
+        }
+
+        const reply = await getAiResponse(message);
+
+        thread.messages.push({ role: 'model', content: reply });
+        thread.updatedAt = new Date();
+
+        await thread.save();
+        res.json({ reply: reply});
+
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
 export default router;
