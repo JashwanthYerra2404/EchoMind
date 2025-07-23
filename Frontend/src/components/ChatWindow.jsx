7ba2cdf987ea32c906aaa17727c31d9c3cac9624
JashwanthYerra2404 <jashwanthyerra2404@gmail.com>
2025-07-23 18:27:09 +0530

    Completed chatwindow

--- Frontend/src/components/ChatWindow.jsx
+++ Frontend/src/components/ChatWindow.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import './ChatWindow.css' 
+import Chat from './Chat.jsx'
 
 const ChatWindow = () => {
   return (
-    <div>
-      ChatWindow
+    <div className='chat-window'>
+
+      {/* Navbar */}
+      <div className='navbar'>
+        <span>ECHOMIND <i class="fa-solid fa-angle-down"></i></span>
+        <div className="usericondiv">
+          <span><i class="fa-solid fa-user"></i></span>
+        </div>
+      </div>
+
+      {/* Chats */}
+      <Chat/>
+      {/* Chat input */}
+      <div className="chat-input">
+        <div className='user-input'>
+          <input placeholder='Ask anything' type="text" name="" id="" />
+          <div id='submit'><i class="fa-solid fa-paper-plane"></i></div>
+        </div>
+        <p></p>
+      </div>
     </div>
   )
 }
