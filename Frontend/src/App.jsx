27f8b1c9ef7a0c3b7b7c4212edb8cd81b947c781
JashwanthYerra2404 <jashwanthyerra2404@gmail.com>
2025-07-23 12:37:00 +0530

    Frontend initial commit with basic structure and components

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Sidebar from './components/Sidebar.jsx'
+import ChatWindow from './components/ChatWindow.jsx'
+import Chat from './components/Chat.jsx'
+
+
+function App() {
+
+  return (
+    <div>
+      <Sidebar />
+      <ChatWindow />
+      <Chat />
+    </div>
+  )
+}
+
+export default App
